66740a948157d353fa0e3009ced9d82c28c78e88
mklewandowski <matt.kain.lewandowski@gmail.com>
2023-08-25 15:06:40 -0500

    Update text and layout

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -51,11 +51,18 @@ public class GameSceneManager : MonoBehaviour
 
     int tutorialNum = 0;
     string[] tutorialStrings = {
-        "Computers store data as binary numbers.\n\nBinary numbers are made up of 0s and 1s.\n\nThey looks like this:\n\n00101\n10110\n11001",
-        "Computers send data back and forth across the world. Sometimes this data gets corrupted.\n\nHow does a computer tell if data is good or bad? How does a computer tell if data is corrupted?\n\nOne method is a parity bit.",
-        "A parity bit is a 0 or 1 added to the end of a binary number. For example, using even parity, a 0 or 1 is added to the end of a binary number so that ALL of the digits add up to an EVEN number.\n\n0001 becomes 00011\n0011 becomes 00110",
-        "When a computer receives data, it checks each binary number. If the digits add up to an even number, the data is likely still good. If the digits add up to an odd number, the data is bad.\n\n00011 is GOOD because...\n0+0+1+1=2 which is EVEN\n\n0010 is BAD because...\n0+0+1+0=1 which is ODD",
-        "Now you try! A parity bit has been added to our binary numbers. The sum of the digits should be an even number. Act fast and decide if the information is good or bad.\n\nReady?"
+        "Imagine you and your friends are passing secret notes in class using a complex code of letters, and you want to make sure that neither of you messes up the message. You decide on a rule that the count of the letters in your message is always an <b><color=#EEAA8F>even number.</color></b>\n\nIf your message has an odd number of letters, you add an extra <b><color=#EEAA8F>Z</color></b> at the end. If it has an even number of letters, you don’t add anything.\n\nNow, when your friend gets the note, they can quickly count the total letters. If the count is even it matches the rule and they know the message is correct. If the count is odd, they know something went wrong when writing the message.",
+
+        "<b><color=#EEAA8F>Parity bits</color></b> at the end of digital information work the same way as that <b><color=#EEAA8F>extra Z</color></b>. Most things that you want to send or receive wirelessly these days, like a photo or a text, are encoded into binary format. This means the information is turned into a string of 0s and 1s.\n\nA parity bit at the end of a wireless signal helps the computer know it received the correct message. If the computer uses <b><color=#EEAA8F>even parity</color></b> to check for errors the rule is simple: the total number of 1s in the data (including the parity bit) should always be an even number.",
+
+        "Let's say the data to send is: 1101.\n\n<b><color=#EEAA8F>Count the 1s:</color></b> In 1101, there are three 1s.\n<b><color=#EEAA8F>Determine the Parity Bit:</color></b> Since 3 is an odd number, and our rule is that we want an even number of 1s, the sender needs to add a parity bit with the value of 1 to make the total count even.\n<b><color=#EEAA8F>The Data to Send:</color></b> So, with the parity bit, the data becomes: 11011.",
+
+        "Data to send is: 1101.\n\nNow, on the receiving side.\n\n<b><color=#EEAA8F>Receive and Count:</color></b> The computer gets the data 11011 and counts the 1s. There are four 1s.\n<b><color=#EEAA8F>Check the Parity Rule:</color></b> 4 is even, which is correct for the even parity rule. So, the computer knows that there were no errors in this data transmission.",
+
+        "<b><color=#EEAA8F>But what if an error occurs?</color></b>\nWhat if due to some interference, the third bit accidentally changes from 0 to 1 during transmission? Now the receiver gets the data 11111.\n\n<b><color=#EEAA8F>Receive and Count:</color></b> The receiver counts five 1s.\n\n<b><color=#EEAA8F>Check the Parity Rule:</color></b> 5 is odd, which breaks our even parity rule. The receiver now knows that there was an error in transmission!\nWhile a parity bit can help receivers detect an error in a transmission, it doesn't tell a computer where the error occurred or how to correct it. More advanced error-checking methods can help with those problems.",
+
+        "<b><color=#EEAA8F>Now you are the computer receiving the data!</color></b> A parity bit has been added to a string of binary numbers. The sum of the digits should be an even number. Act fast and decide if the digital information is good or bad.\n\nReady?"
+
     };
 
 
